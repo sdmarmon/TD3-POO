a24dfb1511460a8abb52ae06dce136bac5936587
TomVallee <tom.vallee37@gmail.com>
2017-11-06 11:19:40 +0100

    Commit Domain 3 Classes

--- Domain/Guest.cs
+++ Domain/Guest.cs
@@ -7,5 +7,37 @@ namespace Domain
 {
     public class Guest
     {
+        //proprietes
+        public string Country { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string LastName { get; set; }
+
+
+        //constructeurs
+        public Guest()
+        {
+            Country = "France";
+            Firstname = "Chel";
+            LastName = "Mi";
+        }
+
+        public Guest(string lastn, string firstn, string country)
+        {
+            Country = country;
+            Firstname = firstn;
+            LastName = lastn;
+        }
+
+        //methodes
+        public override string ToString()
+        {
+            string st = "Guest : ";
+            st += "\nNom : " + LastName + " Prenom : " + Firstname ;
+            st += "\nPays : " + Country;
+            return st;
+        }
+
     }
 }
