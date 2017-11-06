fb4f66182858d1bc56a94d2d12a00845d0375bdc
Sylvain Duhau-marmon <sdmarmon@ensc.fr>
2017-11-06 11:23:11 +0100

    Implémentation interfaces DAL Signed-off-by: Sylvain Duhau-marmon <sdmarmon@ensc.fr>

---
+++ DAL/ReservationRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using Domain;
+
+namespace DAL
+{
+    class ReservationRepository : IReservationRepository
+    {
+        private List<Reservation> reservationList;
+        private string filePath;
+
+        public ReservationRepository()
+        {
+            filePath = @"reservations.xml";
+            if (File.Exists(filePath))
+            {
+                StreamReader reader = new StreamReader(filePath);
+                reservationList = (List<Reservation>)new XmlSerializer(typeof(List<Room>)).Deserialize(reader);
+                reader.Close();
+            }
+            else
+            {
+                // Fill the list with sample data
+                reservationList = new List<Reservation>();
+            }
+        }
+
+        public List<Reservation> GetAll()
+        {
+            return reservationList;
+        }
+    }
+}
